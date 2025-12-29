4852646df489a43f8c767540030912846c5efc07
Minahil Ali <minahilali272@gmail.com>
2025-12-29 22:42:40 +0500

    Fix PHP parse error in functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,29 @@
 <?php
-// Theme functions will go here
-<?php
 // Enqueue theme styles
 function rtcamp_enqueue_styles() {
     wp_enqueue_style(
-        'rtcamp-style', // Handle name
-        get_stylesheet_uri(), // Automatically points to style.css
-        array(), // Dependencies
-        '1.0', // Version
-        'all' // Media
+        'rtcamp-style',
+        get_stylesheet_uri(),
+        array(),
+        '1.0',
+        'all'
     );
 }
 add_action('wp_enqueue_scripts', 'rtcamp_enqueue_styles');
+
+// Enqueue theme scripts
+function rtcamp_enqueue_scripts() {
+    // Load jQuery (WordPress built-in)
+    wp_enqueue_script('jquery');
+
+    // Load custom JS
+    wp_enqueue_script(
+        'rtcamp-script',
+        get_template_directory_uri() . '/js/main.js',
+        array('jquery'),
+        '1.0',
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'rtcamp_enqueue_scripts');
+
